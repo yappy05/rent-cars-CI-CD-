6d424c9351bb612b2d187c4bf9b3d7ac630c3bab
sergey <wendingnn@gmail.com>
2025-06-15 17:25:44 +0300

    init repo

---
+++ src/main/java/com/example/carsharing/model/User.java
@@ -0,0 +1,6 @@
+package com.example.carsharing.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
---
+++ src/test/java/com/example/carsharing/CarsharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
