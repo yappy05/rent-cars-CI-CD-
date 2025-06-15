afed6a18c0e9ba21ed20e558e882d92c7fb54ea0
sergey <wendingnn@gmail.com>
2025-06-15 17:37:00 +0300

    user model

--- src/main/java/com/example/carsharing/model/User.java
+++ src/main/java/com/example/carsharing/model/User.java
@@ -1,6 +1,20 @@
 package com.example.carsharing.model;
 
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String id;
+}
